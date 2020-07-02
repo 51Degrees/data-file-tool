e5a12f0f4ef263d6f96a60da048d12b17c5fd098
Steve Ballantine <Stephen@51degrees.com>
2020-07-02 11:47:59 +0100

    LIC: Added license file and source notices.

--- DataFileTool/App.xaml.cs
+++ DataFileTool/App.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/* *********************************************************************
+ * This Source Code Form is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2019 51 Degrees Mobile Experts Limited, 9 Greyfriars Rd, 
+ * Reading, Berkshire, RG1 1NU.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ *
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ ********************************************************************** */
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
