aa5552bf83c8cf6895474903806818fcb3a4c7c6
James Rosewell <james@51degrees.com>
2023-10-12 10:02:37 +0100

    CLEANUP: Removed unused using, added x64/AnyCPU across solution.

--- DataFileTool/MainWindow.xaml.cs
+++ DataFileTool/MainWindow.xaml.cs
@@ -14,21 +14,11 @@
  ********************************************************************** */
 
 using DataFileHeader;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 
 namespace DataFileTool
 {
